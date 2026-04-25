01a6d4838fc89e1c9b30e1310ad679bb2d4d1847
Sangwoo <ksp0334@gmail.com>
2026-04-26 00:28:02 +0900

    feat: 음력 변환 기능 추가 및 관련 테스트 작성
    - korean_lunar_utils 패키지를 추가하여 음력 변환 기능 구현
    - LunarDisplayUtils 클래스를 생성하여 양력 날짜에 대한 음력 캡션 포맷팅 기능 추가
    - DayEventsBottomSheet에서 음력 날짜 표시를 기존 더미 데이터에서 새로운 기능으로 변경
    - 음력 변환 기능에 대한 단위 테스트 작성

---
+++ test/lunar_display_utils_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:poj_todo/core/utils/lunar_display_utils.dart';
+
+void main() {
+  group('LunarDisplayUtils.formatLunarCaption', () {
+    test('패키지 예시와 동일한 양→음 표기', () {
+      expect(
+        LunarDisplayUtils.formatLunarCaption(DateTime(2019, 8, 14)),
+        '음력 7.14',
+      );
+      expect(
+        LunarDisplayUtils.formatLunarCaption(DateTime(2020, 5, 3)),
+        '음력 4.11',
+      );
+      expect(
+        LunarDisplayUtils.formatLunarCaption(DateTime(2023, 8, 9)),
+        '음력 6.23',
+      );
+    });
+
+    test('음력 연도가 양력 연도와 다르면 음력 연도 포함', () {
+      expect(
+        LunarDisplayUtils.formatLunarCaption(DateTime(2026, 1, 1)),
+        '음력 2025.11.13',
+      );
+    });
+
+    test('지원 범위 밖은 안내 문구', () {
+      expect(
+        LunarDisplayUtils.formatLunarCaption(DateTime(1899, 12, 31)),
+        '음력 (1900~2049만 표시)',
+      );
+      expect(
+        LunarDisplayUtils.formatLunarCaption(DateTime(2050, 2, 1)),
+        '음력 (1900~2049만 표시)',
+      );
+    });
+  });
+}
